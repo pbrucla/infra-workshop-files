444b3451689ecf121a2306efb93bd7bd1c7f6d50
burturt <31748545+burturt@users.noreply.github.com>
2025-01-03 10:45:17 -0800

    minor fixes

--- Intro_To_Kubernetes/act1/soln/activity_1.sh
+++ Intro_To_Kubernetes/act1/soln/activity_1.sh
@@ -15,7 +15,9 @@ psql -U workshop -d workshop -h 127.0.0.1 -c 'SELECT * from secrets;'
 
 # Extra testing
 kubectl exec -it deployment/db -n myapp -- psql -U workshop -d workshop -h 127.0.0.1 -c "INSERT INTO secrets VALUES ('test');"
+sleep 1
+kubectl exec -it deployment/db -n myapp -- psql -U workshop -d workshop -h 127.0.0.1 -c 'SELECT * from secrets;'
 kubectl rollout restart -n myapp deployment/db
 sleep 10
-kubectl exec -it deployment/db -n myapp -- psql -U workshop -d workshop -h 127.0.0.1 -c "INSERT INTO secrets VALUES ('test');"
+kubectl exec -it deployment/db -n myapp -- psql -U workshop -d workshop -h 127.0.0.1 -c 'SELECT * from secrets;'
 kubectl delete -f act_1_soln.yaml
