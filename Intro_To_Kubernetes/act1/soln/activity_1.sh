4af2e45273e4c3bf53b21efd4fc38f451f48991d
burturt <31748545+burturt@users.noreply.github.com>
2025-01-03 10:43:33 -0800

    minor fixes

--- Intro_To_Kubernetes/act1/soln/activity_1.sh
+++ Intro_To_Kubernetes/act1/soln/activity_1.sh
@@ -7,7 +7,15 @@ kubectl apply -f act_1_soln.yaml
 kubectl get deployments -n myapp
 sleep 10
 kubectl get deployments -n myapp
-kubectl logs deployment/db
+kubectl logs deployment/db -n myapp
 kubectl exec -it deployment/db -n myapp -- psql -U workshop -d workshop -h 127.0.0.1 -c 'SELECT * from secrets;'
 kubectl port-forward deployment/db -n myapp 5432:5432&
+sleep 3
 psql -U workshop -d workshop -h 127.0.0.1 -c 'SELECT * from secrets;'
+
+# Extra testing
+kubectl exec -it deployment/db -n myapp -- psql -U workshop -d workshop -h 127.0.0.1 -c 'INSERT INTO secrets VALUES ('test');'
+kubectl rollout restart -n myapp deployment/db
+sleep 10
+kubectl exec -it deployment/db -n myapp -- psql -U workshop -d workshop -h 127.0.0.1 -c 'INSERT INTO secrets VALUES ('test');'
+kubectl delete -f act_1_soln.yaml
